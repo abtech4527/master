58311a5de8e40fcce0f5b2f774349d0b41598bf2
abtech4527 <asbenavides@romaisd.com>
2022-04-29 13:33:45 -0500

    Create JoinDC.ps1

---
+++ JoinDC.ps1
@@ -0,0 +1,5 @@
+$dc = "ROMAISD" # Specify the domain to join.
+$pw = "Adriel2016!" | ConvertTo-SecureString -asPlainText –Force # Specify the password for the domain admin.
+$usr = "$dc\asbenavides" # Specify the domain admin account.
+$creds = New-Object System.Management.Automation.PSCredential($usr,$pw)
+Add-Computer -DomainName $dc -Credential $creds -restart -force -verbose # Note that the computer will be restarted automatically.
