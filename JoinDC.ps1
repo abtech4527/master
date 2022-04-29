a9b1a771c21dfc473b0e6dbf59082ee6d2da19b3
abtech4527 <asbenavides@romaisd.com>
2022-04-29 14:32:03 -0500

    Update JoinDC.ps1

--- JoinDC.ps1
+++ JoinDC.ps1
@@ -1,5 +1,5 @@
 $domain = "romaisd.com" # Specify the domain to join.
-$password = Read-Host -Prompt "Enter password for $username" | ConvertTo-SecureString -asPlainText -force # Specify the password for the domain admin.
 $user = "$domain/asbenavides" # Specify the domain admin account.
+$password = "Adriel2016!" | ConvertTo-SecureString -asPlainText -force # Specify the password for the domain admin.
 $credential = New-Object System.Management.Automation.PSCredential($user,$password)
 Add-Computer -DomainName $domain -Credential $credential -restart -force -verbose # Note that the computer will be restarted automatically.
