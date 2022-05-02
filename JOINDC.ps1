ab206fc7fa39ba8da4aeaea16987248dc445b97d
abtech4527 <asbenavides@romaisd.com>
2022-05-02 13:59:03 -0500

    Update JOINDC.ps1

--- JOINDC.ps1
+++ JOINDC.ps1
@@ -1,6 +1,6 @@
-$domain = "romaisd.com"
-$user = "bhernandez"
-$password = "Tech1432" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\$user"
-$credential = New-Object System.Management.Automation.PSCredential($user,$password)
-Add-Computer -DomainName $domain -Credential $credential -verbose Restart-Computer
+$DMN = "romaisd.com"
+$username = "asbenavides"
+$user = "$DMN\$username"
+$password = "Adriel3190!" | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($user,$password)
+Add-Computer -Credential $credential -DomainName $DMN -Confirm -Restart -Force
