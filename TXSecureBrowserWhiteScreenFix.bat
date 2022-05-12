fec6899fb9389b4756efad4f1e5a25dd8dbc6ff0
abtech4527 <asbenavides@romaisd.com>
2022-05-12 08:21:44 -0500

    Create TXSecureBrowserWhiteScreenFix.bat

---
+++ TXSecureBrowserWhiteScreenFix.bat
@@ -0,0 +1,7 @@
+rmdir /s /q C:\Users\administrator\AppData\Local\CAI\TXSecureBrowser14.0
+
+rmdir /s /q C:\Users\administrator\AppData\Roaming\CAI\TXSecureBrowser14.0
+
+rmdir /s /q C:\Users\testnav\AppData\Local\CAI\TXSecureBrowser14.0
+
+rmdir /s /q C:\Users\testnav\AppData\Roaming\CAI\TXSecureBrowser14.0
