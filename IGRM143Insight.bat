98cc3785da50c6a588bbb05e5cf7da64123c7ccd
abtech4527 <asbenavides@romaisd.com>
2022-08-25 11:11:56 -0500

    Update IGRM143Insight.bat

--- IGRM143Insight.bat
+++ IGRM143Insight.bat
@@ -1,3 +1,3 @@
 @echo off
 
-msiexec.exe /i "C:\Student\Student.msi" /qn ADVANCED_OPTIONS=1 SECURE_MODE=0 CHANNEL=143 ENABLECHANNELSELECT=1
+msiexec.exe /i "C:\Student\Student.msi" /qn ADVANCED_OPTIONS=1 SECURE_MODE=0 CHANNEL=124 ENABLECHANNELSELECT=1
