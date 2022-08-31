dc92a705e03df7395cffc13fd83d827d6201b750
abtech4527 <asbenavides@romaisd.com>
2022-08-31 15:26:14 -0500

    Update LogOnOnlineTesting.bat

--- LogOnOnlineTesting.bat
+++ LogOnOnlineTesting.bat
@@ -2,4 +2,4 @@
 
 Set username for logon: reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d asbenavides
 Set users password: reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d Adriel3190!
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoLogonCount /t REG_DWORD /d 0
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoLogonCount /t REG_DWORD /d 1
