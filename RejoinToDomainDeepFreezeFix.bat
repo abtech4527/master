305f270a2dcf766d89fa645137f1e34a2de1242d
abtech4527 <asbenavides@romaisd.com>
2023-02-24 09:34:38 -0600

    Update RejoinToDomainDeepFreezeFix.bat

--- RejoinToDomainDeepFreezeFix.bat
+++ RejoinToDomainDeepFreezeFix.bat
@@ -1,7 +1,7 @@
 @echo off
-set domain = romaisd.com
-set username = romaisd.com\asbenavides
-set password = Adriel3190!
+set domain=<romaisd.com>
+set username=<romaisd.com\asbenavides>
+set password=<Adriel3190!>
 ECHO Resetting domain password.
 powershell.exe -Command "& {Reset-ComputerMachinePassword -Server %domain% -Credential (New-Object System.Management.Automation.PSCredential ('%username%',(ConvertTo-SecureString '%password%' -AsPlainText -Force)))}"
 ECHO Script Complete
