ec8a398ea1398666ea6f8c5e7ed606ea35a72705
abtech4527 <asbenavides@romaisd.com>
2023-03-01 15:10:59 -0600

    Update DomainTrustFIX.ps1

--- DomainTrustFIX.ps1
+++ DomainTrustFIX.ps1
@@ -1,5 +1,4 @@
 $domain= "adm-vm-dc01.romaisd.com"
 $User= "romaisd.com\asbenavides"
 $Pass = ConvertTo-SecureString "Adriel3190!" -AsPlainText -Force
-
 Reset-ComputerMachinePassword -Server $domain -Credential ((New-Object System.Management.Automation.PSCredential($User, $Pass )))
