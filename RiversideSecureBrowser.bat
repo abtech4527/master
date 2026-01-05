8b1dd93d870dbab6c394b2dc63c268abbfa5828b
abtech4527 <asbenavides@romaisd.com>
2026-01-05 14:09:14 -0600

    Update RiversideSecureBrowser.bat

--- RiversideSecureBrowser.bat
+++ RiversideSecureBrowser.bat
@@ -1,21 +1,15 @@
 @echo off
 REM Define variables
-SET SHARE_PATH=\\192.168.119.137\Technology_Software\DataManager.msi
-SET USERNAME=.\asbenavides
-SET PASSWORD=Adriel3190!
-SET MSI_FILE=DataManager.msi
-
+SET SHARE_PATH=\\ADM-VM-DC01\Shares\Software$\
 
 REM Authenticate and connect to the network share, mapping it to a temporary drive letter (e.g., Z:)
 REM The /persistent:no flag ensures the mapping is removed after the script finishes
-NET USE Z:"%SHARE_PATH%" %PASSWORD% /USER:%USERNAME%  /persistent:no
-IF %ERRORLEVEL% NEQ 0 (
-    echo Failed to connect to network share. Exiting.
-    goto end
-)
+
+NET USE Z: %SHARE_PATH%  /persistent:no
 
 REM Install the MSI file from the mapped drive silently (/qn) with logging (/L*V)
-start /wait msiexec.exe /i "%SHARE_PATH%" /qb
+
+start /wait msiexec.exe /i "\\ADM-VM-DC01\Shares\Software$\RiversideSecureBrowser.msi" /qb
 
 REM Disconnect the network drive
 NET USE Z: /delete
