0380888acedbfacf82b5ea8bdebeb9c5f5e5d8ca
abtech4527 <asbenavides@romaisd.com>
2026-01-05 11:49:05 -0600

    Update RiversideSecureBrowser.bat

--- RiversideSecureBrowser.bat
+++ RiversideSecureBrowser.bat
@@ -1,7 +1,24 @@
 @echo off
-REM Define the network path to the MSI file
-SET "MSIPATH=\\192.168.119.137\Technology_Software\Update Softwares\DataManager.msi"
+REM Define variables
+SET SHARE_PATH=\\192.168.119.137\Technology_Software\Update Softwares\DataManager.msi
+SET USERNAME=asbenavides
+SET PASSWORD=Adriel3190!
+SET MSI_FILE=Installer.msi
 
 
-REM Run the MSI installation silently and wait for it to complete
-start "MSI Installer" /wait msiexec.exe /i "%MSIPATH%" /qb
+REM Authenticate and connect to the network share, mapping it to a temporary drive letter (e.g., Z:)
+REM The /persistent:no flag ensures the mapping is removed after the script finishes
+NET USE Z: "%SHARE_PATH%" %PASSWORD% /USER:%USERNAME% /persistent:no
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to connect to network share. Exiting.
+    goto end
+)
+
+REM Install the MSI file from the mapped drive silently (/qn) with logging (/L*V)
+start /wait msiexec.exe /i "Z:\%MSI_FILE%" /qn /L*V "%LOG_FILE%"
+
+REM Disconnect the network drive
+NET USE Z: /delete
+
+:end
+echo Installation process finished.
