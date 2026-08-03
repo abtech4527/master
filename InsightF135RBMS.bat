9395583debe65e30dd7d592d08f4b8b93bd60d77
abtech4527 <asbenavides@romaisd.com>
2026-08-03 14:47:29 -0500

    Create InsightF135RBMS.bat

---
+++ InsightF135RBMS.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Define variables
+SET SHARE_PATH=\\ADM-VM-DC01\Shares\Software$\
+
+REM Authenticate and connect to the network share, mapping it to a temporary drive letter (e.g., Z:)
+REM The /persistent:no flag ensures the mapping is removed after the script finishes
+
+NET USE Z: %SHARE_PATH%  /persistent:no
+
+REM Install the MSI file from the mapped drive silently (/qn) with logging (/L*V)
+
+start /wait msiexec.exe /i "\\ADM-VM-DC01\Shares\Software$\InsightStudentSetup.msi" /qb ADVANCED_OPTIONS=1 SECURE_MODE=0 CHANNEL=135 ENABLECHANNELSELECT=1
+
+REM Disconnect the network drive
+NET USE Z: /delete
+
+:end
+echo Installation process finished.
